7809f78957543ea1e0a0a7467f506db10bd54352
Pankaj Nelson Tirkey <pankajnelsontirkey@outlook.com>
2019-08-12 21:00:13 +0530

    Commit from personal system

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,31 +1,23 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
-import { Recipe } from "./recipe.model";
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
-      "First recipe",
-      "First description",
-      "https://upload.wikimedia.org/wikipedia/commons/8/89/7126_-_Luzern_-_Chicken_%26_pasta.JPG",
-      [
-        new Ingredient("Tomatoes", 4),
-        new Ingredient("Onions", 2),
-        new Ingredient("Apples", 2)
-      ]
+      'First recipe',
+      'First description',
+      'https://upload.wikimedia.org/wikipedia/commons/8/89/7126_-_Luzern_-_Chicken_%26_pasta.JPG',
+      [new Ingredient('Tomatoes', 4), new Ingredient('Onions', 2), new Ingredient('Apples', 2)]
     ),
     new Recipe(
-      "Second recipe",
-      "Second description",
-      "https://upload.wikimedia.org/wikipedia/commons/8/89/7126_-_Luzern_-_Chicken_%26_pasta.JPG",
-      [
-        new Ingredient("Potatoes", 2),
-        new Ingredient("Onions", 4),
-        new Ingredient("Tomatoes", 3)
-      ]
+      'Second recipe',
+      'Second description',
+      'https://upload.wikimedia.org/wikipedia/commons/8/89/7126_-_Luzern_-_Chicken_%26_pasta.JPG',
+      [new Ingredient('Potatoes', 2), new Ingredient('Onions', 4), new Ingredient('Tomatoes', 3)]
     )
   ];
 
